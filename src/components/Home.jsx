98a42eca37e521c269b010deb9315c191a868e1e
Islomjon Ergashev <76722837+ergashevislomjon@users.noreply.github.com>
2023-03-02 15:29:28 +0500

    add text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
   return (
     <div className="h-[100vh] flex flex-col">
       <HomeHeader />
+    {alert("Bu googleni clonei")}
       <main className="grow flex justify-center">
         <div className="w-full px-5 flex flex-col items-center mt-44">
           <img className="w-[172px] md:w-[272px] mb-8" src={Logo} alt="Logo" />
